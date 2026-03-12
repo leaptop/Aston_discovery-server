90674750dbfa0378b59889c970338857df5fcd6e
Stepan csl <cslpnj@gmail.com>
2026-03-12 17:33:21 +0300

    initiated the Eureka for service discovery

--- src/main/java/com/alekseev/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/com/alekseev/discoveryserver/DiscoveryServerApplication.java
@@ -1,7 +1,20 @@
 package com.alekseev.discoveryserver;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
- * @author stepa
- * @since 2026-03-12
- */public class DiscoveryServerApplication {
+ * Главный класс сервера обнаружения (Eureka).
+ *
+ * @EnableEurekaServer включает функциональность реестра сервисов.
+ * Другие микросервисы будут регистрироваться здесь, указывая своё имя и адрес.
+ * Eureka также предоставляет веб-интерфейс на порту 8761 для мониторинга.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
 }
